3f808a5efd232ce77df46af4979a02a62686abe4
vasilyl1 <likhovaido@gmail.com>
2023-03-20 18:42:45 -0400

    header and footer update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Project from './components/tabs/Portfolio';
 import Footer from './components/Footer';
 
 // In the main App component, only single instances of Header, Navigtion and Footer
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,11 +2,52 @@ import React from 'react';
 // importing CSS file as a dependency
 import '../styles/Style.css';
 
-function Footer() {
+function Footer({ currentPage, handlePageChange }) {
   return (
-    <header className="header">
-      <h1>Footer</h1>
-    </header>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="https://github.com/vasilyl1"
+          onBlur={() => handlePageChange('GitHub')}
+          // if the current page is "GitHub"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'GitHub' ? 'nav-link active' : 'nav-link'}
+        >
+          GitHub
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="https://www.linkedin.com/in/vlikhovaido/"
+          onBlur={() => handlePageChange('LinkedIn')}
+          className={currentPage === 'GitHub' ? 'nav-link active' : 'nav-link'}
+        >
+          LinkedIn
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#e-mail"
+          onBlur={() => handlePageChange('Email')}
+          // if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'Email' ? 'nav-link active' : 'nav-link'}
+        >
+          likhovaido@gmail.com
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#phone"
+          onBlur={() => handlePageChange('Phone')}
+          // if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'Phone' ? 'nav-link active' : 'nav-link'}
+        >
+          705.300.0993
+        </a>
+      </li>
+      </ul>
   );
 }
 
