a338f06b86807ea962fd10a0a5daefa53af22b4a
vasilyl1 <likhovaido@gmail.com>
2023-04-10 10:22:59 -0400

    crypto team project addition

--- client/src/components/data/portfolio.js
+++ client/src/components/data/portfolio.js
@@ -1,12 +1,20 @@
 
 const projects = [
     {
+        name: 'Crypto Coin',
+        description: 'An App for the beginners thinking to join the crypto world',
+        repo:'https://github.com/vasilyl1/crypto-coin',
+        deployment: 'https://cryptocoin.herokuapp.com/',
+        tech:'REACT/MERN/Apollo/JavaScript/JWT/Bcrypt/GraphQL/Bootstrap',
+        class:'n2body-right-2 box2'
+    },
+    {
         name: 'My Career® Personal Development',
         description: 'Personal Development Content Management System. Team project',
         repo:'https://github.com/vasilyl1/my-career',
         deployment: 'https://my-career.herokuapp.com/',
         tech:'Handlebars/JavaScript/CSS/Node/Express/Sequelize/MySQL/AI',
-        class:'n2body-right-2 box2'
+        class:'n2body-right-3 box3'
     },
     {
         name: 'SportLink Connect',
@@ -14,7 +22,7 @@ const projects = [
         repo:'https://github.com/vasilyl1/sport-link-connect',
         deployment: 'https://vasilyl1.github.io/sport-link-connect/',
         tech:'HTML/CSS/JavaScript',
-        class:'n2body-right-3'
+        class:'n2body-right-4'
     },
     {
         name: 'CMS Blog',
@@ -22,7 +30,7 @@ const projects = [
         repo:'https://github.com/vasilyl1/CMS-blog',
         deployment: 'https://cms-blog-vl.herokuapp.com/',
         tech:'Handlebars/ JavaScript/ CSS/Node/ Express/ Sequelize/ MySQL/Bcrypt',
-        class:'n2body-right-4 box4'
+        class:'n2body-right-5 box5'
     },
     {
         name: 'Work Day Scheduler',
@@ -30,7 +38,7 @@ const projects = [
         repo:'https://github.com/vasilyl1/work-day-scheduler/',
         deployment: 'https://vasilyl1.github.io/work-day-scheduler/',
         tech:'Handlebars/ JavaScript/ CSS/Node/ Express/ Sequelize/ MySQL/Bcrypt',
-        class:'n2body-right-5 box5'
+        class:'n2body-right-6 box6'
     },
     {
         name: 'Weather Dashboard',
@@ -38,7 +46,7 @@ const projects = [
         repo:'https://github.com/vasilyl1/weather-dashboard',
         deployment: 'https://vasilyl1.github.io/weather-dashboard',
         tech:'API/JavaScript/CSS',
-        class:'n2body-right-6 box6'
+        class:'n2body-right-7 box7'
     }
 ];
 export {projects};
--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 module.exports = (app) =>
-  app.get('/*', (req, res) =>
+  app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '..', '/..', '/client', '/build', '/index.html'))
   );
